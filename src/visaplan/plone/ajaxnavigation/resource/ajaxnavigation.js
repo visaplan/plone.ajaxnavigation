e826abc5f9d09d118d9af570ceee9be8cb1c492d
Tobias Herp <tobias.herp@stein.de>
2019-05-29 09:55:47 +0200

    Added profiles and first ajaxnavigation.js
    ajaxnavigation.js for now contains still test code;
    it is added to the Javascript registry (and removed upon uninstallation
    of the package)

---
+++ src/visaplan/plone/ajaxnavigation/resource/ajaxnavigation.js
@@ -0,0 +1,84 @@
+﻿/* visaplan.plone.ajaxnavigation: AJAX navigation for Plone sites
+ * Copyright (C) 2019  visaplan GmbH, Bochum, Germany
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+var AjaxNav = (function () {
+    var me = {},
+        log = function (txt) {
+            if (console && console.log) {
+                console.log(txt);
+            }
+        };
+    me.log = log;
+	var url = window.location.href;
+	var pos1 = url.indexOf('://');
+	var tmp = url.slice(pos1+3); // alles ab dem Hostnamen
+	var pos2 = tmp.indexOf('/');
+	var hostname: tmp.slice(3, tmp.indexOf('/'));
+	me.rooturl =  url.slice(0, pos1 + 3 + pos2);
+	alert('root-URL ist '+me.rooturl);
+	var clickfunc = function (e) {
+		log('AjaxNav.click :-)');
+		log(e)
+		alert(e);
+		log(this);
+		if (! confirm('OK für AJAX-Laden; Abbruch für undelegate:')) {
+			$(this).undelegate('click', clickfunc);
+		}
+	}
+	me.click = clickfunc
+	return me;
+})();
+AjaxNav.init = function (key) {
+	if (key === undefined) {
+		key = 'default';
+	}
+	$.ajax({
+		dataType: 'json',
+		url: rooturl+'/ajaxnav-options-'+key,
+		success: function (data, textStatus, jqXHR) {
+			AjaxNav.log('AjaxNav.init('+key+') received data:');
+			AjaxNav.log(data);
+			AjaxNav.options = data;
+
+			var thelist = data.whitelist,
+				selector;
+			if (thelist === undefined) {
+				thelist = ['body'];
+			}
+			for (var i=0; i < thelist.length; i++) {
+				selector = thelist[i]
+				$(selector).delegate('a', 'click', AjaxNav.click);
+			}
+			thelist = data.blacklist;
+			if (thelist !== undefined) {
+				for (var i=0; i < thelist.length; i++) {
+					selector = thelist[i]
+					$(selector).undelegate('a', 'click', AjaxNav.click);
+				}
+			}
+			AjaxNav.log('AjaxNav.init('+key+') completed.');
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			AjaxNav.log('E:AjaxNav.init('+key+') --> Error '+textStatus+':');
+			AjaxNav.log(jqXHR);
+			if (errorThrown !== undefined) {
+				AjaxNav.log(errorThrown);
+			}
+			alert('AjaxNav.init: Error '+textStatus+' for key "'+key+'" :-(');
+		}
+	});
+}
+// -*- coding: utf-8 -*- vim: ts=4 sw=4 sts=4 noet ai noic tw=79 cc=+1
---
+++ src/visaplan/plone/ajaxnavigation/setuphandlers.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Organisatorisches:
+__author__ = """Tobias Herp <tobias.herp@visaplan.com>"""
+__docformat__ = 'plaintext'
+from Products.CMFPlone.interfaces import INonInstallable
+from zope.interface import implementer
+
+# Sonstiges (Plone):
+from Products.CMFCore.utils import getToolByName
+from plone.app.upgrade.utils import loadMigrationProfile
+
+# Unitracc-Tools:
+from visaplan.plone.tools.setup import (
+        step,
+        )
+
+# Logging:
+import logging
+# ------------------------------------------------------ [ Daten ... [
+# UNITRACC_PORTAL_TYPES_1000 = UNITRACC_PORTAL_TYPES[:17]
+
+PROJECTNAME = __package__
+PROFILE_ID = PROJECTNAME + ':default'
+LOGGER_LABEL = PROJECTNAME + ': setuphandlers'
+# ------------------------------------------------------ ] ... Daten ]
+
+logger = logging.getLogger(LOGGER_LABEL)
+
+
+@implementer(INonInstallable)
+class HiddenProfiles(object):
+
+    def getNonInstallableProfiles(self):
+        """Hide uninstall profile from site-creation and quickinstaller"""
+        return [
+            PROJECTNAME + ':uninstall',
+        ]
+
+
+def post_install(context):
+    """Post install script"""
+    logger.info('Installation complete')
+
+    # Do something at the end of the installation of this package.
+
+
+def uninstall(context):
+    """Uninstall script"""
+    # Do something at the end of the uninstallation of this package.
+
+
+# ------------------------ [ Migrationsschritte, ./profiles.zcml ... [
+# ------------------------ ] ... Migrationsschritte, ./profiles.zcml ]
