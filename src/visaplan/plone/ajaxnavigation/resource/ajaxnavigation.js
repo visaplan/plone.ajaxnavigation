31f0c32e2d370c27ac7df14614b2cb61265c15eb
Tobias Herp <tobias.herp@stein.de>
2019-06-05 18:42:44 +0200

    Cleaner formatting
    ("function" is not the name of the function;
    https://www.crockford.com/code.html)

--- src/visaplan/plone/ajaxnavigation/resource/ajaxnavigation.js
+++ src/visaplan/plone/ajaxnavigation/resource/ajaxnavigation.js
@@ -144,12 +144,12 @@ var AjaxNav = (function () {
 	AjaxNav.hostname_mismatch = hostname_mismatch;
 
 	// from https://www.joezimjs.com/javascript/3-ways-to-parse-a-query-string-in-a-url/
-	var parseQueryString = function( queryString ) {
+	var parseQueryString = function (queryString) {
 		var params = {}, queries, temp, i, l;
 		// Split into key/value pairs
 		queries = queryString.split("&");
 		// Convert the array of strings into an object
-		for ( i = 0, l = queries.length; i < l; i++ ) {
+		for (i = 0, l = queries.length; i < l; i++) {
 			temp = queries[i].split('=');
 			params[temp[0]] = temp[1];
 		}
