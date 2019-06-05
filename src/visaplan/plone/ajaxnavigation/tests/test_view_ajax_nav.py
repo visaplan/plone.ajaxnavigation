3761b762d6a1d53573fa35ebb421a5757cc345b5
Tobias Herp <tobias.herp@stein.de>
2019-06-05 12:46:10 +0200

    "Add view: ajax-nav"

---
+++ src/visaplan/plone/ajaxnavigation/tests/test_view_ajax_nav.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from visaplan.plone.ajaxnavigation.testing import VISAPLAN_PLONE_AJAXNAVIGATION_FUNCTIONAL_TESTING
+from visaplan.plone.ajaxnavigation.testing import VISAPLAN_PLONE_AJAXNAVIGATION_INTEGRATION_TESTING
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getMultiAdapter
+from zope.component.interfaces import ComponentLookupError
+
+import unittest
+
+
+class ViewsIntegrationTest(unittest.TestCase):
+
+    layer = VISAPLAN_PLONE_AJAXNAVIGATION_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Folder', 'other-folder')
+        api.content.create(self.portal, 'Collection', 'my-collection')
+
+    def test_ajax_nav_is_registered(self):
+        view = getMultiAdapter(
+            (self.portal['other-folder'], self.portal.REQUEST),
+            name='ajax-nav'
+        )
+        self.assertTrue(view(), 'ajax-nav is not found')
+        self.assertTrue(
+            'Sample View' in view(),
+            'Sample View is not found in ajax-nav'
+        )
+
+    def test_ajax_nav_in_my_collection(self):
+        with self.assertRaises(ComponentLookupError):
+            getMultiAdapter(
+                (self.portal['my-collection'], self.portal.REQUEST),
+                name='ajax-nav'
+            )
+
+
+class ViewsFunctionalTest(unittest.TestCase):
+
+    layer = VISAPLAN_PLONE_AJAXNAVIGATION_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
