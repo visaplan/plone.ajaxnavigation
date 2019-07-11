093a0b0d40b377b29dcd641491e1060a44c1a86b
Tobias Herp <tobias.herp@stein.de>
2019-07-11 17:21:54 +0200

    Textual corrections
    package name

--- src/visaplan/plone/ajaxnavigation/locales/update.sh
+++ src/visaplan/plone/ajaxnavigation/locales/update.sh
@@ -3,10 +3,10 @@
 # ``export PATH=$PATH:$BUILDOUT_DIR/bin`` when i18ndude is located in your buildout's bin directory)
 #
 # For every language you want to translate into you need a
-# locales/[language]/LC_MESSAGES/visaplan.plone.animations.po
-# (e.g. locales/de/LC_MESSAGES/visaplan.plone.animations.po)
+# locales/[language]/LC_MESSAGES/visaplan.plone.ajaxnavigation.po
+# (e.g. locales/de/LC_MESSAGES/visaplan.plone.ajaxnavigation.po)
 
-domain=visaplan.plone.animations
+domain=visaplan.plone.ajaxnavigation
 
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
