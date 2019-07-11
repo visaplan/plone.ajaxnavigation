7e7ec47425279901661ea6622e132ff01109469f
Tobias Herp <tobias.herp@stein.de>
2019-07-11 10:07:42 +0200

    Removed value_type options
    ... because of ValueError exceptions (zope.schema 4.2.2)
    Furthermore: correction of (copied) default value.

--- src/visaplan/plone/ajaxnavigation/interfaces.py
+++ src/visaplan/plone/ajaxnavigation/interfaces.py
@@ -12,9 +12,10 @@ from visaplan.plone.ajaxnavigation import _
 class IAjaxNavigationClientSettings(model.Schema):
     """ Schema fields for view "@@ajaxnav-options-default"
     """
+    # no value_type options currently, because of
+    # ValueError: 'value_type' must be field instance.
     whitelist = schema.List(
         title=_(u"Whitelist"),
-        value_type=schema.BytesLine,
         default=['body',
                  ],
         description=_(u"CSS selectors which activate delegation"
@@ -22,14 +23,12 @@ class IAjaxNavigationClientSettings(model.Schema):
 
     blacklist = schema.List(
         title=_(u"Blacklist"),
-        value_type=schema.BytesLine,
         default=[],
         description=_(u"CSS selectors which deactivate delegation"
         " for their <a> descendants"))
 
     nested_blacklist = schema.Bool(
         title=_(u"Nested blacklist"),
-        value_type=schema.BytesLine,
         default=False,
         description=_(u"If this is set to True, and if there is a non-empty"
         " Blacklist, the Blacklist un-delegation will be applied during the"
@@ -37,7 +36,6 @@ class IAjaxNavigationClientSettings(model.Schema):
 
     view_ids = schema.List(
         title=_(u"View_ids"),
-        value_type=schema.BytesLine,
         default=['view',
                  'edit',
                  'base_edit'],
@@ -45,7 +43,6 @@ class IAjaxNavigationClientSettings(model.Schema):
 
     view_prefixes = schema.List(
         title=_(u"View prefixes"),
-        value_type=schema.BytesLine,
         default=['manage_',
                  ],
         description=_(u"If the last path segment of a URL starts with one of"
@@ -53,7 +50,6 @@ class IAjaxNavigationClientSettings(model.Schema):
 
     view_suffixes = schema.List(
         title=_(u"View suffixes"),
-        value_type=schema.BytesLine,
         default=['_view',
                  ],
         description=_(u"If the last path segment of a URL ends with one of the"
@@ -61,7 +57,6 @@ class IAjaxNavigationClientSettings(model.Schema):
 
     blacklist_view_ids = schema.List(
         title=_(u"Blacklist_view_ids"),
-        value_type=schema.BytesLine,
         default=['manage',
                  'edit',
                  'base_edit'],
@@ -71,13 +66,11 @@ class IAjaxNavigationClientSettings(model.Schema):
 
     blacklist_view_prefixes = schema.List(
         title=_(u"Blacklist_view_prefixes"),
-        value_type=schema.BytesLine,
         default=['manage_',
                  ])
 
     blacklist_view_suffixes = schema.List(
         title=_(u"Blacklist_view_suffixes"),
-        value_type=schema.BytesLine,
         default=['_edit',
                  ])
 
@@ -85,11 +78,10 @@ class IAjaxNavigationClientSettings(model.Schema):
         title=_(u"Selectors for data keys"),
         default={'content': ["#region-content,#content"],
                  },
+        key_type=schema.BytesLine,
         description=_(u'For each "normal" key from an AJAX response, '
         'configure a CSS selector which tells where to put the value. '
-        'You can give more than one value, separated by comma.'),
-        key_type=schema.BytesLine,
-        value_type=schema.BytesLine)
+        'You can give more than one value, separated by comma.'))
 
 
 class IAjaxNavigationInternalSettings(model.Schema):
@@ -97,13 +89,11 @@ class IAjaxNavigationInternalSettings(model.Schema):
     """
     layout4ajax = schema.Dict(
         title=_(u"Layouts for AJAX"),
-        default={'content': ["#region-content,#content"],
-                 },
+        default={},
+        key_type=schema.BytesLine,
         description=_(u'For each "normal" layout of a link target, '
         'you may configure an AJAX version which will provide the '
-        '"meat" only'),
-        key_type=schema.BytesLine,
-        value_type=schema.BytesLine)
+        '"meat" only'))
 
 
 class IVisaplanPloneAjaxnavigationLayer(IDefaultBrowserLayer):
